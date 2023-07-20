349e2d0d164e45ef1e6f096911f76f621f732bb4
Thouda Ruchitha <135091095+Ruchitha0204@users.noreply.github.com>
2023-07-20 11:13:00 +0530

    Create Flaskapp.py

---
+++ Flaskapp.py
@@ -0,0 +1,33 @@
+// Install required Python Packages
+flask
+
+pip install flask
+twilio
+
+pip install twilio
+
+
+
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
